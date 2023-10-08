dd560f642f73e12c60346958e6c7e6dcbbaaccbb
samajestecodeur <issa97403@gmail.com>
2023-10-08 13:29:54 +0200

    first12

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from .stats import *
 
 urlpatterns = [
     path('donneescollectees/', DonneeCollecteeListCreateView.as_view(), name='donnee-collectee-list-create'),
@@ -11,4 +12,27 @@ urlpatterns = [
     path('canal/', CanalListView.as_view(), name='canal'),
     path('marque/', MarqueListView.as_view(), name='marque'),
     path('site/', SiteListView.as_view(), name='site'),
+
+    # Statistiques générales
+    path('collecte/', TotalCollectedDataView.as_view(), name='general-statistics'),
+    # # Statistiques par emplacement géographique
+    # path('api/statistiques/geographique/', AverageSurfaceView.as_view(), name='geographical-statistics'),
+    # # Statistiques par type de support publicitaire
+    # path('api/statistiques/support-publicitaire/', AverageDurationView.as_view(), name='support-statistics'),
+    # # Statistiques sur la taxation
+    # path('api/statistiques/taxation/', TotalAdsByTypeView.as_view(), name='taxation-statistics'),
+    # # Statistiques par emplacement géographique
+    # path('api/statistiques/geographique/', TotalAdTaxView.as_view(), name='geographical-statistics'),
+    # # Statistiques par type de support publicitaire
+    # path('api/statistiques/support-publicitaire/', TotalODPView.as_view(), name='support-statistics'),
+    # # Statistiques sur la taxation
+    # path('api/statistiques/taxation/', AverageAdTaxPerMonthView.as_view(), name='taxation-statistics'),
+    # # Statistiques sur l'utilisation de l'application
+    # path('api/statistiques/utilisation/', TotalAgentsView.as_view(), name='application-usage-statistics'),
+    # # Statistiques sur l'état des supports publicitaires
+    # path('api/statistiques/etat-support/', CollectsPerAgentView.as_view(), name='support-condition-statistics'),
+    # # Statistiques sur la visibilité
+    # path('api/statistiques/visibilite/', LastCollectionDatePerAgentView.as_view(), name='visibility-statistics'),
+    # # Statistiques par propriétaire
+    # path('api/statistiques/proprietaire/', TotalAdsByStateView.as_view(), name='owner-statistics'),
 ]
