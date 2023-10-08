b993d9ae8c1206ca961bcfc3e8a6e7bcaa160990
issa <issa97403@gmail.com>
2023-10-08 07:02:15 +0000

    firstaud6

--- api/views.py
+++ api/views.py
@@ -7,21 +7,17 @@ from .models import *
 from .serializers import *
 
 class DonneeCollecteeListCreateView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = DonneeCollectee.objects.all()
     serializer_class = DonneeCollecteeSerializer
 
 class DonneeCollecteeDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = DonneeCollectee.objects.all()
     serializer_class = DonneeCollecteeSerializer
 
 class SupportPublicitaireListView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = SupportPublicitaire.objects.all()
     serializer_class = SupportPublicitaireSerializer
 
 class SupportPublicitaireDetailView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = SupportPublicitaire.objects.all()
     serializer_class = SupportPublicitaireSerializer
