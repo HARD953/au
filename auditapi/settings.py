71d6456cb07744c83012cf92d182fc16a4a19e93
samajestecodeur <issa97403@gmail.com>
2023-11-08 21:16:07 +0100

    der12

--- auditapi/settings.py
+++ auditapi/settings.py
@@ -155,19 +155,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 AUTH_USER_MODEL = 'custumer.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
-    "https://127.0.0.1:8000",
-    "http://localhost:8000",
-    "https://localhost:8000",
-    "http://127.0.0.1:9000",
-    "https://auditvisibilite.lanfiasave.com/",
-    "http://auditvisibilite.lanfiasave.com/"
-    ]
+CORS_ALLOWED_ORIGINS = ['*']
 
 CORS_ALLOW_METHODS = [
     "DELETE",
