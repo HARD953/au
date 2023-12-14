851eb1e29a4df814fc799049ca6daff2025abf0b
samajestecodeur <issa97403@gmail.com>
2023-12-14 23:47:02 +0100

    donnee1

--- auditapi/settings.py
+++ auditapi/settings.py
@@ -122,9 +122,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
 ], 
-# 'DEFAULT_AUTHENTICATION_CLASSES': [
-#      'rest_framework_simplejwt.authentication.JWTAuthentication'
-#     ],
+'DEFAULT_AUTHENTICATION_CLASSES': [
+     'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # Nombre d'éléments par page
 }
