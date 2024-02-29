74d66b2ba9be748bfb178c26c41bcc39d6cb4d0a
samajestecodeur <issa97403@gmail.com>
2024-02-29 20:04:15 +0100

    last 12

--- api/urls.py
+++ api/urls.py
@@ -37,24 +37,4 @@ urlpatterns = [
     path('gcollecte/<str:start_date>/<str:end_date>/', GTotalCollectedDataView.as_view(), name='general-statistics'),
     path('collecte/<str:start_date>/<str:end_date>/', TotalCollectedDataView.as_view(), name='statistics-etat'),
     path('collectem/<str:start_date>/<str:end_date>/', GTotalCollectedDataViewM.as_view(), name='statistics-marque'),
-    # Statistiques par emplacement géographique
-    # path('api/statistiques/geographique/', AverageSurfaceView.as_view(), name='geographical-statistics'),
-    # # Statistiques par type de support publicitaire
-    # path('api/statistiques/support-publicitaire/', AverageDurationView.as_view(), name='support-statistics'),
-    # # Statistiques sur la taxation
-    # path('api/statistiques/taxation/', TotalAdsByTypeView.as_view(), name='taxation-statistics'),
-    # # Statistiques par emplacement géographique
-    # path('api/statistiques/geographique/', TotalAdTaxView.as_view(), name='geographical-statistics'),
-    # # Statistiques par type de support publicitaire
-    # path('api/statistiques/support-publicitaire/', TotalODPView.as_view(), name='support-statistics'),
-    # # Statistiques sur la taxation
-    # path('api/statistiques/taxation/', AverageAdTaxPerMonthView.as_view(), name='taxation-statistics'),
-    # # Statistiques sur l'utilisation de l'application
-    # path('api/statistiques/utilisation/', TotalAgentsView.as_view(), name='application-usage-statistics'),
-    # # Statistiques sur l'état des supports publicitaires
-    # path('api/statistiques/etat-support/', CollectsPerAgentView.as_view(), name='support-condition-statistics'),
-    # # Statistiques sur la visibilité
-    # path('api/statistiques/visibilite/', LastCollectionDatePerAgentView.as_view(), name='visibility-statistics'),
-    # # Statistiques par propriétaire
-    # path('api/statistiques/proprietaire/', TotalAdsByStateView.as_view(), name='owner-statistics'),
 ]
--- custumer/serializers.py
+++ custumer/serializers.py
@@ -14,7 +14,7 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'adresse', 'district', 'region', 'departement', 'sous_prefecture', 'commune','profile_image','is_agent','is_recenseur','is_entreprise','entreprise')
+        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'adresse', 'district', 'region', 'departement', 'sous_prefecture', 'commune','profile_image','is_agent','is_recenseur','is_entreprise','entreprise','is_lanfia')
         extra_kwargs ={
             'password':{'write_only':True}
         }
